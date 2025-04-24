39d33a7fd3c36c23c08e7ee820260e3aed9d5e12
ASHAN8BUDDHIMAL <161815763+ASHAN8BUDDHIMAL@users.noreply.github.com>
2025-04-24 15:18:09 +0530

    User login

--- src/main/java/com/example/demo/model/Login.java
+++ src/main/java/com/example/demo/model/Login.java
@@ -1,4 +1,16 @@
 package com.example.demo.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
