f8212c6d85fd00002d288b875c1da9deb696dba9
ASHAN8BUDDHIMAL <161815763+ASHAN8BUDDHIMAL@users.noreply.github.com>
2025-05-08 10:21:47 +0530

    User login & registration

--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -1,8 +1,11 @@
 package com.example.demo.Service;
+
 import com.example.demo.model.UserRegistration;
 import com.example.demo.repository.RegUser;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -12,12 +15,19 @@ public class UserService {
     private RegUser regUser;
 
     public UserRegistration create(UserRegistration userReg) {
-
         return regUser.save(userReg);
-
     }
 
     public List<UserRegistration> getAllUsers() {
         return regUser.findAll();
     }
+    public UserRegistration findByEmailAndPassword(String email, String password) {
+        return regUser.findByEmailAndPassword(email, password)
+                .orElse(null); // return null if no match is found
+    }
+    public User findByEmail(String email) {
+        return regUser.findByEmail(email);
+    }
+
+
 }
--- src/main/java/com/example/demo/repository/RegUser.java
+++ src/main/java/com/example/demo/repository/RegUser.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository;
 import com.example.demo.model.UserRegistration;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface RegUser extends JpaRepository<UserRegistration, Long> {
+    Optional<UserRegistration> findByEmailAndPassword(String email, String password);
+    User findByEmail(String email);
 
     }
 
